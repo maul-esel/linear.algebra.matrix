7563a07908443288905136c5f08c30561e4aba00
maul.esel <maul.esel@go4more.de>
2014-06-07 19:31:35 +0200

    interpreter: inject ResourceSet instead of using explicit implementation

--- linear.algebra.matrix.interpreter/src/linear/algebra/matrix/interpreter/Program.java
+++ linear.algebra.matrix.interpreter/src/linear/algebra/matrix/interpreter/Program.java
@@ -7,13 +7,12 @@ import com.google.inject.Injector;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 
 public class Program {
 	public static void main(String[] args) {
 		Injector injector = new MatrixStandaloneSetup().createInjectorAndDoEMFRegistration();
 
-		ResourceSet rs = new ResourceSetImpl();
+		ResourceSet rs = injector.getInstance(ResourceSet.class);
 		Resource resource = rs.getResource(URI.createURI(args[0]), true);
 
 		Interpreter interpreter = injector.getInstance(InterpreterFactory.class).create(resource);
